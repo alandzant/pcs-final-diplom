27034fe7f0b2925886087c246affc9da8236a52a
alandzant <127549234+alandzant@users.noreply.github.com>
2023-07-09 16:15:29 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -9,9 +10,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            System.out.println("Идет соединение с сервером на порту 8989");
             BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-
-            Scanner scanner = new Scanner(System.in);
 
             while (true) {
                 try (
@@ -22,18 +22,17 @@ public class Main {
                     String request = in.readLine();
                     String answer;
 
-                    // Ввод слова для поиска
-                    System.out.print("Введите слово для поиска: ");
-                    String word = scanner.nextLine();
-
                     // Поиск и формирование ответа
-                    if (word.isEmpty()) {
+                    if (request.isEmpty()) {
                         answer = "Слово не введено.";
                     } else {
-                        answer = engine.search(word).toString();
+                        engine.search(request).stream().forEach(jsonObject -> {
+                            System.out.println(jsonObject);
+                        });
+                        continue;
                     }
 
-                    System.out.println(answer);
+                    out.println(answer);
                 }
             }
         } catch (IOException e) {
